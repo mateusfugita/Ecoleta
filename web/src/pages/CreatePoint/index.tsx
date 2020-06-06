49e061fc16ed570ae44321b9bf8091b4f76d7aa4
Mateus Fugita <mateushiwao@gmail.com>
2020-06-06 01:06:23 -0300

    add CreatePoint page

---
+++ web/src/pages/CreatePoint/index.tsx
@@ -0,0 +1,108 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
+import { Map, TileLayer, Marker } from 'react-leaflet';
+
+import './styles.css';
+import logo from '../../assets/logo.svg';
+
+const CreatePoint = () => {
+    return(
+        <div id="page-create-point">
+            <header>
+                <img src={logo} alt='Ecoleta'></img>
+
+                <Link to='/'>
+                    <FiArrowLeft />
+                    Voltar para home
+                </Link>
+            </header>
+
+            <form>
+                <h1>Cadastro do <br /> ponto de coleta</h1>
+
+                <fieldset>
+                    <legend>
+                        <h2>Dados</h2>
+                    </legend>
+
+                    <div className="field">
+                        <label htmlFor='name'>Nome da entidade</label>
+                        <input
+                            type='text'
+                            name='name'
+                            id='name'
+                        ></input>
+                    </div>
+
+                    <div className="field-group">
+                    <div className="field">
+                        <label htmlFor='email'>E-mail</label>
+                        <input
+                            type='email'
+                            name='email'
+                            id='email'
+                        ></input>
+                    </div>
+                    <div className="field">
+                        <label htmlFor='whatsapp'>Whatsapp</label>
+                        <input
+                            type='text'
+                            name='whatsapp'
+                            id='whatsapp'
+                        ></input>
+                    </div>
+                    </div>
+                </fieldset>
+
+                <fieldset>
+                    <legend>
+                        <h2>Endereço</h2>
+                        <span>Selecione o endereço no mapa</span>
+                    </legend>
+
+                    <Map center={[-23.711152, -46.585077]} zoom={15}>
+                        <TileLayer
+                            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+
+                        <Marker position={[-23.711152, -46.585077]}></Marker>
+                    </Map>
+
+                    <div className="field-group">
+                        <div className="field">
+                            <label htmlFor="uf">Estado (UF)</label>
+                            <select name="uf" id="uf">
+                                <option value="0">Selecione uma UF</option>
+                            </select>
+                        </div>
+                        <div className="field">
+                            <label htmlFor="city">Cidade</label>
+                            <select name="city" id="city">
+                                <option value="0">Selecione uma cidade</option>
+                            </select>
+                        </div>
+                    </div>
+                </fieldset>
+
+                <fieldset>
+                    <legend>
+                        <h2>Itens de coleta</h2>
+                        <span>Selecione um ou mais itens abaixo</span>
+                    </legend>
+
+                    <ul className="items-grid">
+                        <li>
+                            <img src='' alt='Teste'></img>
+                        </li>
+                    </ul>
+                </fieldset>
+
+                <button type='submit'>Cadastrar o ponto de coleta</button>
+            </form>
+        </div>
+    )
+}
+
+export default CreatePoint;
